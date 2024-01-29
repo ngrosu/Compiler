b1e53a5af325a2a715e6139ad5b1aec05371301e
nimrod <nimrod.usorg@gmail.com>
2024-01-29 22:08:42 +0200

    some basic headers for a short first session

--- Compiler/Lexer/dfa_table.h
+++ Compiler/Lexer/dfa_table.h
@@ -26,9 +26,10 @@ void add_transition(DFA, char, short); // Add a transition to the transition tab
 
 void add_token_to_dfa(DFA, char[]); // Set up the transitions for a token in the transition table
 
-void set_state(DFA, short, );
+void set_state(DFA, short, token_type); // Set a state in the states array to the appropriate token
 
-void set_state_alnum_to_identifier(DFA, char);
+void set_state_alnum_to_identifier(DFA, char); // Set transitions to all alnum characters
+// except for one to the identifier state
 
 
 #endif //COMPILER_DFA_TABLE_H
