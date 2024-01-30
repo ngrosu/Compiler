fe74c3dcee7222b45de1854ba2b14309ea8b031e
nimrod <nimrod.usorg@gmail.com>
2024-01-30 14:43:00 +0200

    nothing much here

--- Compiler/Lexer/dfa_table.c
+++ Compiler/Lexer/dfa_table.c
@@ -5,4 +5,31 @@
 #include "dfa_table.h"
 
 
+DFA create_DFA()
+{
+    DFA dfa = malloc(sizeof(_DFA));
+    dfa->num_of_states=1;
+    dfa->states=calloc(dfa->num_of_states,sizeof(token_type));
+    dfa->transition_matrix=calloc(dfa->num_of_states,sizeof(short)*NUM_OF_CHARS);
+    return dfa;
+}
 
+void init_DFA(DFA dfa)
+{
+}
+
+void allocate_states(DFA dfa, short state_pos)
+{
+
+}
+
+void add_transition(DFA dfa, char input, short state, short transition)
+{
+    if (!(dfa->num_of_states>state && dfa->num_of_states>transition))
+    {
+        dfa->num_of_states = (state>transition ? state : transition);
+        dfa->states = realloc(dfa->states, (dfa->num_of_states+1)*sizeof(token_type));
+        dfa->transition_matrix = realloc(dfa->transition_matrix,
+                                         (dfa->num_of_states+1)*sizeof(short)*NUM_OF_CHARS)
+    }
+}
--- Compiler/Lexer/dfa_table.h
+++ Compiler/Lexer/dfa_table.h
@@ -3,28 +3,30 @@
 //
 
 #include "lexer.h"
+#include <stdlib.h>
 
 #ifndef COMPILER_DFA_TABLE_H
 #define COMPILER_DFA_TABLE_H
-#define DFA_MAX_STATES 256
 #define NUM_OF_CHARS 128
 
 typedef struct {
 
-    token_type states[DFA_MAX_STATES];
+    token_type *states;
     short (*transition_matrix)[NUM_OF_CHARS];
-    short num_of_transitions;
+    short num_of_states;
     short current_state;
 
-} *DFA;
+} *DFA, _DFA;
 
 DFA create_DFA(); // Create and return an instance of the state machine
 
 void init_DFA(DFA); // Initialize the state machine
 
-void add_transition(DFA, char, short); // Add a transition to the transition table
+void allocate_states(DFA, short)  // Allocate the memory required to the
 
-void add_token_to_dfa(DFA, char[]); // Set up the transitions for a token in the transition table
+void add_transition(DFA, char, short, short); // Add a transition to the transition table
+
+void add_token_to_DFA(DFA, char[]); // Set up the transitions for a token in the transition table
 
 void set_state(DFA, short, token_type); // Set a state in the states array to the appropriate token
 
